317532a1f5cf1d11066dd7f966d942a156004260
Nicolae Semitar <Nicolae.Semitar@endava.com>
2020-10-08 20:24:33 +0300

    Third Commit - Updated Task Fifth

--- src/practiceTask1/Java_Core_Fifth_Task.java
+++ src/practiceTask1/Java_Core_Fifth_Task.java
@@ -9,6 +9,14 @@ JAVA AM Intern
  */
 public class Java_Core_Fifth_Task {
 
+    // recursie
+//    public static int factorial(int n) {
+//        if (n <= 1)
+//            return 1;
+//        int fact = n * factorial(n - 1);
+//        return fact;
+//    }
+
     public static void reverse(int[] arr, int k) {
         if (k <= 1)
             return;
@@ -22,6 +30,33 @@ public class Java_Core_Fifth_Task {
             arr[k - i - 1] = aux;
         }
     }
+    // metoda auxiliara recursiva
+    private static void recsort(int[] arr, int k) {
+        if (k == 1)
+            return;
+        // recursie
+        recsort(arr, k - 1);
+        if (arr[k - 1] <= arr[0]) {
+            reverse(arr, k - 1);
+            reverse(arr, k);
+        }
+        else if (arr[k - 1] >= arr[k - 2])
+            return;
+        else {
+            reverse(arr, k);
+            int i = 1;
+            while (arr[0] < arr[i])
+                i++;
+            reverse(arr, i);
+            reverse(arr, i - 1);
+            reverse(arr, k);
+        }
+    }
+
+    public static int[] sort(int[] arr) {
+        recsort(arr, arr.length);
+        return arr;
+    }
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -32,6 +67,17 @@ public class Java_Core_Fifth_Task {
 
         // a)
         reverse(array, 5);
-        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < n; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+
+        // b)
+        array = sort(array);
+        for (int i = 0; i < n; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
     }
 }
+
+
+
